a39a880fe37b08b719c4366065cf1ba7a7f57e63
gabe <GabeMaker@users.noreply.github.com>
2014-11-27 14:35:14 +0000

    calcs printing without heading text

--- afewthingstotry.rb
+++ afewthingstotry.rb
@@ -1,8 +1,5 @@
-puts “how many hours are in a year?”
 puts 24 * 365
 
-puts “how many minutes are in a decade?”
 puts ((60*(24*365))*10) + ((60*24)*2)
 
-puts “how many seconds old are you (at 30 with 7 leap years)?”
 puts (60*60*24*365*30) + (60*60*24*7)
