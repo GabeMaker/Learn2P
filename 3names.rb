c182319d3da765632b4239a007d8573801a62dad
gabe <GabeMaker@users.noreply.github.com>
2014-11-28 10:18:44 +0000

    chap4 exercises + deleted test file

---
+++ 3names.rb
@@ -0,0 +1,7 @@
+puts 'Hello, what is your first name?'
+name = gets.chomp
+puts 'and your surname?'
+sname = gets.chomp
+puts 'what about your middle name?'
+mname = gets.chomp
+puts 'Well, it is very nice to meet you ' + name + ' ' + mname + ' ' + sname + '!'
---
+++ fave.rb
@@ -0,0 +1,4 @@
+puts "what\'s your favourite number?"
+n = gets.chomp.to_i
+nplus = n + 1
+puts "I'm sorry, but I'm afraid with all due respect, " + nplus.to_s + " is a much bigger and better favourite number"
