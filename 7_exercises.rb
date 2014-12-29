26a419813f56fc06c0bc371d5449d637bcfb01c3
gabe <GabeMaker@users.noreply.github.com>
2014-12-29 01:05:54 +0000

    Chris Pine refactoring of Leap Years

--- 7_exercises.rb
+++ 7_exercises.rb
@@ -1,18 +1,20 @@
 # 3 - Leap Years After refactoring
 
 puts 'what is the starting year?'
-year = gets.chomp.to_i
+start_year = gets.chomp.to_i
 puts 'what is the ending year?'
 end_year = gets.chomp.to_i
 puts "---"
-puts "leap years between #{year} and #{end_year} are:"
+puts "leap years between #{start_year} and #{end_year} are:"
 
-
-while year < end_year
-  if year % 4 == 0
-    if year % 100 != 0 || year % 400 == 0
-      puts year
-    end
-  end
-  year += 1
+(start_year..end_year).each do |year|
+  next if year%4 != 0
+  next if year%100 == 0 && year%400 != 0
+  puts year
 end
+
+# next is an Enumerator method eg. 
+# (0..5).each do |x|
+# next if x%2 != 0
+# puts x
+# end
